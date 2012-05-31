c0c05609f4b1468585a8f29fb8696a1195bec4c0
Tango <Tango.S.Fleuri@Gmail.com>
2012-05-31 22:04:03 +0800

    Initial_C

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SuperExaApp::Application.config.secret_token = '5833ddd891759fd96c21b68226c55e4fe38dfef86955accbdf17b2ba7bce8a94252c2dd4a1af53c6a09cf4847ff7a2f53f69ca7742844ed0628d4c99de6ed742'
