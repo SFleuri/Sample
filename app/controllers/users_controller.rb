0c9400f3f1c8d9c76589903356c66272d9e195ff
Tango <Tango.S.Fleuri@Gmail.com>
2012-06-13 20:37:08 +0800

    Avant pagination~~~

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-	
+	before_filter :signed_in_user, only: [:edit, :index, :update]
+	before_filter :correct_user, only: [:edit, :update]
+
 	def show
 		@user = User.find(params[:id])
 	end
@@ -18,5 +20,39 @@ class UsersController < ApplicationController
 			render 'new'
 		end
 	end
+
+	def edit
+		#@user = User.find(params[:id])
+	end
+
+	def update
+		#@user = User.find(params[:id])
+		if @user && @user.update_attributes(params[:user])
+			flash[:success] = "Updated!"
+			sign_in @user
+			redirect_to @user
+		else
+			render 'edit'
+		end
+	end
+
+	def index
+		@user = User.all
+	end
+
+	private
+
+	def signed_in_user
+		unless signed_in?
+			store_location
+			redirect_to signin_path, notice:"Signed in?"
+		end
+	end
+
+	def correct_user
+		@user = User.find(params[:id])
+		redirect_to(root_path) unless current_user?(@user)
+	end
 end
+
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,6 +10,11 @@ module SessionsHelper
 		!current_user.nil?
 	end
 
+	def sign_out
+		self.current_user = nil
+		cookies.delete(:remember_token)
+	end
+
 	def current_user=(user)
 		@current_user = user
 	end
@@ -18,9 +23,17 @@ module SessionsHelper
 		@current_user ||= User.find_by_remember_token(cookies[:remember_token])
 	end
 
-	def sign_out
-		self.current_user = nil
-		cookies.delete(:remember_token)
+	def current_user?(user)
+		user == current_user
+	end
+
+	def redirect_back_or(default)
+		redirect_to(session[:return_to] || default)
+		session.delete(:return_to)
+	end
+
+	def store_location
+		session[:return_to] = request.fullpath
 	end
 end
 
